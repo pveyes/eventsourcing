f016176414419b3f42371b3d44392b3927bc0b12
Fatih Kalifa <fatihkalifa@gmail.com>
2017-03-23 11:14:25 +0700

    fitur menambahkan transaksi secara real-time

--- commerce/public/transaction.js
+++ commerce/public/transaction.js
@@ -1,8 +1,11 @@
 /* global post get setInputReadOnly tambah$ modify$ deleteData$ cancelAction$ changeEditMode$ */
+const ts = new EventSource('/transaction/stream');
+
 const itemData$ = Rx.Observable.fromPromise(get('/item'));
 const customerData$ = Rx.Observable.fromPromise(get('/customer'));
 const bankData$ = Rx.Observable.fromPromise(get('/bank'));
 const allData$ = itemData$.merge(customerData$).merge(bankData$);
+const transactionData$ = Rx.Observable.fromPromise(get('/transaction'));
 
 const $itemOptions = $('#item-list');
 const $customerOptions = $('#customer-list');
@@ -41,7 +44,22 @@ bankData$.subscribe(banks => {
   $bankOptions.append(options);
 });
 
+const transactionRowTemplate = $('#transaction-row-template').text();
+transactionData$.subscribe(transactions => {
+  const rows = transactions.map(transaction => {
+    return transactionRowTemplate
+      .replace(/\$transaction_id/g, transaction.id)
+      .replace('$nama_customer', transaction.namaCustomer)
+      .replace('$alamat_customer', transaction.alamatCustomer)
+      .replace('$nama_item', transaction.namaItem)
+      .replace('$jumlah_item', transaction.jumlahItem)
+      .replace('$harga_item', transaction.hargaItem)
+      .replace('$harga_total', transaction.totalHarga)
+      .replace('$status_transaksi', transaction.status);
+  });
 
+  $transactionTable.append(rows);
+});
 
 
 
@@ -63,7 +81,7 @@ const selectedItemChange$ = Rx.Observable
       });
   });
 
-
+$jumlahOptions = $('#jumlah-item');
 selectedItemChange$.subscribe(item => {
   const stok = parseInt(item.stok);
 
@@ -75,5 +93,120 @@ selectedItemChange$.subscribe(item => {
     options += option;
   }
 
-  $('#jumlah-item').html(options);
+  $jumlahOptions.html(options);
+});
+
+const $beli = $('#buy-item');
+
+const beliItem$ = Rx.Observable
+  .fromEvent($beli, 'click')
+  .flatMap(event => {
+    event.preventDefault();
+    const customerId = parseInt($customerOptions.val());
+    const itemId = parseInt($itemOptions.val());
+    const jumlah = parseInt($jumlahOptions.val());
+    const bankId = parseInt($bankOptions.val());
+
+    const customer$ = customerData$
+      .flatMap(customers => Rx.Observable.from(customers))
+      .filter(customer => customer.id === customerId);
+
+    const item$ = itemData$
+      .flatMap(items => Rx.Observable.from(items))
+      .filter(item => item.id === itemId);
+
+    const bank$ = bankData$
+      .flatMap(banks => Rx.Observable.from(banks))
+      .filter(bank => bank.id === bankId);
+
+    return customer$
+      .merge(item$)
+      .merge(bank$)
+      .reduce((pembelian, val) => {
+        if (val.alamat) {
+          //   customer: {
+          //     id:
+          //     nama:
+          //     alamat:
+          //   },
+          pembelian.customer = {
+            id: val.id,
+            nama: val.nama,
+            alamat: val.alamat,
+          };
+          return pembelian;
+        }
+
+        if (val.harga) {
+          //   item: {
+          //     id:
+          //     nama:
+          //     harga:
+          //   },
+          pembelian.item = {
+            id: val.id,
+            nama: val.nama,
+            harga: val.harga,
+          };
+          return pembelian;
+        }
+
+        if (val.kode) {
+          //   bank: {
+          //     id:
+          //     nama:
+          //     rekening:
+          //     kode:
+          //   }
+          pembelian.bank = {
+            id: val.id,
+            nama: val.nama,
+            rekening: val.rekening,
+            kode: val.kode,
+          };
+          return pembelian;
+        }
+
+        return pembelian;
+      }, {})
+      .map(pembelian => {
+        pembelian.jumlah = jumlah;
+        return pembelian;
+      });
+  })
+  .flatMap(pembelian => {
+    return Rx.Observable.fromPromise(
+      post('/transaction/add', pembelian)
+    )
+  })
+  .filter(serverResponse => {
+    return serverResponse.success === true;
+  });
+
+beliItem$.subscribe(val => {
+  console.log(val);
+});
+
+
+
+const transactionFromSSE$ = Rx.Observable
+  .fromEvent(ts, 'added')
+  .map(event => {
+    const transaction = JSON.parse(event.data);
+    return transaction;
+  })
+
+
+transactionFromSSE$.subscribe(transaction => {
+  const newTransaction = transactionRowTemplate
+    .replace(/\$transaction_id/g, transaction.id)
+    .replace('$nama_customer', transaction.namaCustomer)
+    .replace('$alamat_customer', transaction.alamatCustomer)
+    .replace('$nama_item', transaction.namaItem)
+    .replace('$jumlah_item', transaction.jumlahItem)
+    .replace('$harga_item', transaction.hargaItem)
+    .replace('$harga_total', transaction.totalHarga)
+    .replace('$status_transaksi', transaction.status);
+
+  $transactionTable.append(newTransaction);
 });
--- commerce/server.js
+++ commerce/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const itemHandler = require('./handler/item');
 const customerHandler = require('./handler/customer');
 const bankHandler = require('./handler/bank');
+const transactionHandler = require('./handler/transaction');
 
 const indexHtmlPath = path.resolve('./index.html');
 const transactionHtmlPath = path.resolve('./transaction.html');
@@ -21,12 +22,13 @@ app.get('/stream', sse.init);
 app.use('/item', itemHandler);
 app.use('/customer', customerHandler);
 app.use('/bank', bankHandler);
+app.use('/transaction', transactionHandler);
 
 app.get('/', (req, res) => {
   res.sendFile(indexHtmlPath);
 });
 
-app.get('/transaction', (req, res) => {
+app.get('/transaction-page', (req, res) => {
   res.sendFile(transactionHtmlPath);
 });
 
